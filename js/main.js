e478b17d3f92861c71ee21bc201b56ff69a3687c
unknown <luomel@outlook.com>
2020-03-11 18:09:56 -0400

    Update personal site

--- js/main.js
+++ js/main.js
@@ -118,55 +118,55 @@
 
    /* photoswipe
     * ----------------------------------------------------- */
-    var ssPhotoswipe = function() {
-        var items = [],
-            $pswp = $('.pswp')[0],
-            $folioItems = $('.item-folio');
+    // var ssPhotoswipe = function() {
+    //     var items = [],
+    //         $pswp = $('.pswp')[0],
+    //         $folioItems = $('.item-folio');
 
-        // get items
-        $folioItems.each( function(i) {
+    //     // get items
+    //     $folioItems.each( function(i) {
 
-            var $folio = $(this),
-                $thumbLink =  $folio.find('.thumb-link'),
-                $title = $folio.find('.item-folio__title'),
-                $caption = $folio.find('.item-folio__caption'),
-                $titleText = '<h4>' + $.trim($title.html()) + '</h4>',
-                $captionText = $.trim($caption.html()),
-                $href = $thumbLink.attr('href'),
-                $size = $thumbLink.data('size').split('x'),
-                $width  = $size[0],
-                $height = $size[1];
+    //         var $folio = $(this),
+    //             $thumbLink =  $folio.find('.thumb-link'),
+    //             $title = $folio.find('.item-folio__title'),
+    //             $caption = $folio.find('.item-folio__caption'),
+    //             $titleText = '<h4>' + $.trim($title.html()) + '</h4>',
+    //             $captionText = $.trim($caption.html()),
+    //             $href = $thumbLink.attr('href'),
+    //             $size = $thumbLink.data('size').split('x'),
+    //             $width  = $size[0],
+    //             $height = $size[1];
         
-            var item = {
-                src  : $href,
-                w    : $width,
-                h    : $height
-            }
+    //         var item = {
+    //             src  : $href,
+    //             w    : $width,
+    //             h    : $height
+    //         }
 
-            if ($caption.length > 0) {
-                item.title = $.trim($titleText + $captionText);
-            }
+    //         if ($caption.length > 0) {
+    //             item.title = $.trim($titleText + $captionText);
+    //         }
 
-            items.push(item);
-        });
+    //         items.push(item);
+    //     });
 
-        // bind click event
-        $folioItems.each(function(i) {
+    //     // bind click event
+    //     $folioItems.each(function(i) {
 
-            $(this).on('click', function(e) {
-                e.preventDefault();
-                var options = {
-                    index: i,
-                    showHideOpacity: true
-                }
+    //         $(this).on('click', function(e) {
+    //             e.preventDefault();
+    //             var options = {
+    //                 index: i,
+    //                 showHideOpacity: true
+    //             }
 
-                // initialize PhotoSwipe
-                var lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
-                lightBox.init();
-            });
+    //             // initialize PhotoSwipe
+    //             var lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
+    //             lightBox.init();
+    //         });
 
-        });
-    };
+    //     });
+    // };
 
 
    /* slick slider
@@ -247,7 +247,7 @@
         ssMenuOnScrolldown();
         ssOffCanvas();
         ssMasonryFolio();
-        ssPhotoswipe();
+        // ssPhotoswipe();
         ssSlickSlider();
         ssSmoothScroll();
         ssAlertBoxes();
